ab1b86453a2ac5abfac3dc15848b7359df829eec
Rachel Killackey <rachel@launchpadlab.com>
2016-07-15 12:05:16 -0500

    starting controller specs

---
+++ spec/controllers/contacts_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ContactsController do
+  describe '#update' do
+    pending
+  end
+
+  describe '#show' do
+    pending
+  end
+end
--- spec/controllers/twilio_controller_spec.rb
+++ spec/controllers/twilio_controller_spec.rb
@@ -1,5 +1,28 @@
-# require 'rails_helper'
-#
-# describe TwilioController do
-#
-# end
+require 'rails_helper'
+
+describe TwilioController do
+  describe '.connect' do
+    pending
+  end
+
+  describe '.answer' do
+    pending
+  end
+
+  describe '.complete' do
+    pending
+  end
+
+  describe '.voicemail' do
+    pending
+  end
+
+  describe '.text' do
+    pending
+  end
+
+  describe '.create_contact' do
+    pending
+  end
+
+end
